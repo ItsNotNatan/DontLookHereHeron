c540eadadd2f88666244b788ba501bbf8adc35d1
Natan Guimarães dos Santos <127156404+ItsNotNatan@users.noreply.github.com>
2026-06-10 08:36:03 -0300

    Delete TransportViewerAdm directory

--- TransportViewerAdm/src/App.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './routes'
-
-export default function App() {
-  // 🟢 Renderiza as rotas protegidas e o Layout do Administrador
-  return <RouterProvider router={router} />
-}
