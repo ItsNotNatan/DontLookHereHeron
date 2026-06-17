8b58b533ea5987a372de6c06f0a9246443b63d90
Natan Guimarães dos Santos <127156404+ItsNotNatan@users.noreply.github.com>
2026-06-17 09:15:27 -0700

    Update api.js

--- TransportViewerClient/src/services/api.js
+++ TransportViewerClient/src/services/api.js
@@ -1,3 +1,4 @@
+// src/services/api.js
 import axios from 'axios';
 
 // O Vite vai injetar o IP do .env correto dependendo de onde estiver rodando!
@@ -5,6 +6,19 @@ const baseURL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL
+});
+
+// 🟢 INTERCEPTOR: Anexa o Token em TODAS as requisições automaticamente
+api.interceptors.request.use(async (config) => {
+  const token = localStorage.getItem('accessToken');
+  
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  
+  return config;
+}, (error) => {
+  return Promise.reject(error);
 });
 
 export default api;
