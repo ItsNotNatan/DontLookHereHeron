ae65b40bd5b9e8ce838a987535311a6f8cce0bf7
Natan Guimarães dos Santos <127156404+ItsNotNatan@users.noreply.github.com>
2026-06-19 10:27:14 -0300

    Update projetoRoutes.js

--- BackEnd/src/routes/projetoRoutes.js
+++ BackEnd/src/routes/projetoRoutes.js
@@ -4,6 +4,8 @@ const projetoController = require('../controllers/projetoController');
 const { verificarToken, permitirPerfis } = require('../middlewares/authMiddleware');
 
 router.get('/', projetoController.listarProjetos);
+
+// 🟢 Alteração de perfil: Cadastros base de Projetos (WBS) agora são exclusivos do Admin
 router.post('/', verificarToken, permitirPerfis(['Admin']), projetoController.criarProjeto);
 router.put('/:id', verificarToken, permitirPerfis(['Admin']), projetoController.atualizarProjeto);
 router.delete('/:id', verificarToken, permitirPerfis(['Admin']), projetoController.excluirProjeto);
