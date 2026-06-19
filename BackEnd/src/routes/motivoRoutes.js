0055dcd6aacdeefb0eb54cfc51c53634821d634f
Natan Guimarães dos Santos <127156404+ItsNotNatan@users.noreply.github.com>
2026-06-19 05:45:38 -0700

    Update motivoRoutes.js

--- BackEnd/src/routes/motivoRoutes.js
+++ BackEnd/src/routes/motivoRoutes.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const motivoController = require('../controllers/motivoController');
 const { verificarToken, permitirPerfis } = require('../middlewares/authMiddleware');
 
-router.get('/', verificarToken, permitirPerfis(['Admin', 'Operador', 'Visualizador']), motivoController.listarMotivos);
-router.post('/', verificarToken, permitirPerfis(['Admin', 'Operador']), motivoController.criarMotivo);
-router.put('/:id', verificarToken, permitirPerfis(['Admin', 'Operador']), motivoController.atualizarMotivo);
-router.delete('/:id', verificarToken, permitirPerfis(['Admin', 'Operador']), motivoController.excluirMotivo);
+router.get('/', verificarToken, permitirPerfis(['Admin', 'Operador']), motivoController.listarMotivos);
+// Apenas Admin pode criar/excluir Motivos
+router.post('/', verificarToken, permitirPerfis(['Admin']), motivoController.criarMotivo);
+router.put('/:id', verificarToken, permitirPerfis(['Admin']), motivoController.atualizarMotivo);
+router.delete('/:id', verificarToken, permitirPerfis(['Admin']), motivoController.excluirMotivo);
 
 module.exports = router;
