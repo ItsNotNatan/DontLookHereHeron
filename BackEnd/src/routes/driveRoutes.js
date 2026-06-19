e4fe1ce3e98f43a2c73bab96c0b2c942c543cb5a
Natan Guimarães dos Santos <127156404+ItsNotNatan@users.noreply.github.com>
2026-06-19 10:28:19 -0300

    Update driveRoutes.js

--- BackEnd/src/routes/driveRoutes.js
+++ BackEnd/src/routes/driveRoutes.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const driveController = require('../controllers/driveController');
 const { verificarToken, permitirPerfis } = require('../middlewares/authMiddleware');
 
+// 🟢 Perfil atualizado: O novo Operador pode realizar uploads locais de arquivos
 router.post('/upload', verificarToken, permitirPerfis(['Admin', 'Operador']), driveController.uploadParaDrive);
+
 module.exports = router;
