7d3ab677e4f765394f2d0a35f9589a1a32acf577
Natan Guimarães dos Santos <127156404+ItsNotNatan@users.noreply.github.com>
2026-06-19 10:39:21 -0300

    Update motivoRoutes.js

--- BackEnd/src/routes/motivoRoutes.js
+++ BackEnd/src/routes/motivoRoutes.js
@@ -3,10 +3,7 @@ const router = express.Router();
 const motivoController = require('../controllers/motivoController');
 const { verificarToken, permitirPerfis } = require('../middlewares/authMiddleware');
 
-// 🟢 O novo Operador tem permissão para listar os motivos (Divergências) nas caixas de seleção
 router.get('/', verificarToken, permitirPerfis(['Admin', 'Operador']), motivoController.listarMotivos);
-
-// 🟢 Alteração de perfil: Cadastros base de Motivos agora são exclusivos do Admin
 router.post('/', verificarToken, permitirPerfis(['Admin']), motivoController.criarMotivo);
 router.put('/:id', verificarToken, permitirPerfis(['Admin']), motivoController.atualizarMotivo);
 router.delete('/:id', verificarToken, permitirPerfis(['Admin']), motivoController.excluirMotivo);
