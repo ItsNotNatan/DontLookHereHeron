4d84b27a7cbc8a3e54a770c2c9e7250a89ae99b2
Natan Guimarães dos Santos <127156404+ItsNotNatan@users.noreply.github.com>
2026-06-19 05:46:16 -0700

    Restrict transportadora routes to Admin profile only

--- BackEnd/src/routes/transportadoraRoutes.js
+++ BackEnd/src/routes/transportadoraRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const transportadoraController = require('../controllers/transportadoraController');
 const { verificarToken, permitirPerfis } = require('../middlewares/authMiddleware');
 
-router.get('/', verificarToken, permitirPerfis(['Admin', 'Operador', 'Visualizador']), transportadoraController.listarTransportadoras);
-router.post('/', verificarToken, permitirPerfis(['Admin', 'Operador']), transportadoraController.criarTransportadora);
-router.put('/:id', verificarToken, permitirPerfis(['Admin', 'Operador']), transportadoraController.atualizarTransportadora);
-router.delete('/:id', verificarToken, permitirPerfis(['Admin', 'Operador']), transportadoraController.excluirTransportadora);
+router.get('/', verificarToken, permitirPerfis(['Admin', 'Operador']), transportadoraController.listarTransportadoras);
+router.post('/', verificarToken, permitirPerfis(['Admin']), transportadoraController.criarTransportadora);
+router.put('/:id', verificarToken, permitirPerfis(['Admin']), transportadoraController.atualizarTransportadora);
+router.delete('/:id', verificarToken, permitirPerfis(['Admin']), transportadoraController.excluirTransportadora);
 
 module.exports = router;
