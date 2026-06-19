8edde7cd2c424c73c8a5b11849769d42e2890a32
Natan Guimarães dos Santos <127156404+ItsNotNatan@users.noreply.github.com>
2026-06-19 10:27:45 -0300

    Update localRoutes.js

--- BackEnd/src/routes/localRoutes.js
+++ BackEnd/src/routes/localRoutes.js
@@ -4,7 +4,8 @@ const localController = require('../controllers/localController');
 const { verificarToken, permitirPerfis } = require('../middlewares/authMiddleware');
 
 router.get('/', localController.listarLocais);
-// Apenas Admin pode mexer nos Endereços fixos
+
+// 🟢 Alteração de perfil: Cadastros base de Endereços agora são exclusivos do Admin
 router.post('/', verificarToken, permitirPerfis(['Admin']), localController.criarLocal);
 router.put('/:id', verificarToken, permitirPerfis(['Admin']), localController.atualizarLocal);
 router.delete('/:id', verificarToken, permitirPerfis(['Admin']), localController.excluirLocal);
