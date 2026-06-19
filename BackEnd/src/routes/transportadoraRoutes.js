732ff21cf11c01c3b25d88877b05705015729e1f
Natan Guimarães dos Santos <127156404+ItsNotNatan@users.noreply.github.com>
2026-06-19 10:40:41 -0300

    Update transportadoraRoutes.js

--- BackEnd/src/routes/transportadoraRoutes.js
+++ BackEnd/src/routes/transportadoraRoutes.js
@@ -3,10 +3,7 @@ const router = express.Router();
 const transportadoraController = require('../controllers/transportadoraController');
 const { verificarToken, permitirPerfis } = require('../middlewares/authMiddleware');
 
-// 🟢 O novo Operador tem permissão para ler a lista de transportadoras
 router.get('/', verificarToken, permitirPerfis(['Admin', 'Operador']), transportadoraController.listarTransportadoras);
-
-// 🟢 Alteração de perfil: Cadastros base de Transportadoras agora são exclusivos do Admin
 router.post('/', verificarToken, permitirPerfis(['Admin']), transportadoraController.criarTransportadora);
 router.put('/:id', verificarToken, permitirPerfis(['Admin']), transportadoraController.atualizarTransportadora);
 router.delete('/:id', verificarToken, permitirPerfis(['Admin']), transportadoraController.excluirTransportadora);
